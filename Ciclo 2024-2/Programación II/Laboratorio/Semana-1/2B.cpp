880a3fc8852e508082edb635d94a54c98b5bbd19
Martin59lilo <bonillasarmientomartin@gmail.com>
2025-01-08 15:13:31 -0500

    Estoy subiendo notas de ho

--- Ciclo 2024-2/Programación II/Laboratorio/Semana-1/2B.cpp
+++ Ciclo 2024-2/Programación II/Laboratorio/Semana-1/2B.cpp
@@ -3,7 +3,5 @@
 using namespace std;
 
 int main(){
-    int arreglo_default[2]= {1,2};
-    cout << arreglo_default[1];
-
+    
 }
--- Ciclo 2025-0/Estadística y Probabilidades/Notes.md
+++
@@ -1,2 +0,0 @@
-# Anexos
-Asesorías 2024-1:https://drive.google.com/drive/folders/15_4VszZb84qMCBshvR1_gq5ecfSUBpM_
